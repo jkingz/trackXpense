33c36234b47aed487c7b557044ce3a290bb3b18d
Jkingz <jfkrule@gmail.com>
2025-03-16 04:38:49 +0800

    chore: Add new package react spinners

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react';
+import DashBoardPage from './page';
+import { BarLoader } from 'react-spinners';
+
+const DashBoardLayout = () => {
+  return (
+    <div className="px-5 ">
+      <h1 className="text-6xl font-bold --gradient-title mb-5">Dashboard</h1>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={'100%'} color="#9333ea" />}
+      >
+        <DashBoardPage />
+      </Suspense>
+    </div>
+  );
+};
+
+export default DashBoardLayout;
