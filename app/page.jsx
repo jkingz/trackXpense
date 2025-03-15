52ea7bcd91772775eca600522694ed58b80dc0f8
Jkingz <jfkrule@gmail.com>
2025-03-16 02:06:31 +0800

    style: Adjust how it works styling

--- app/page.jsx
+++ app/page.jsx
@@ -41,18 +41,18 @@ export default function Home() {
         </div>
       </section>
       {/* How It Works Section */}
-      <section className="py-16 bg-white">
+      <section className="py-16 bg-sky-50">
         <div className="container mx-auto text-center px-4">
           <h2 className="text-3xl font-bold text-center mb-12">How it works</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-            {howItWorksData.map((feature, index) => (
-              <Card key={index} className="p-6">
-                <CardContent className="space-y-4 pt-4">
-                  <div className="flex  justify-center">{feature.icon}</div>
-                  <h3 className="text-xl font-semibold">{feature.title}</h3>
-                  <p className="text-gray-600">{feature.description}</p>
-                </CardContent>
-              </Card>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {howItWorksData.map((step, index) => (
+              <div key={index} className="text-center">
+                <div className="flex justify-center items-center w-16 h-16 bg-blue-100 rounded-full mx-auto mb-6">
+                  {step.icon}
+                </div>
+                <h3 className="text-xl font-semibold">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
             ))}
           </div>
         </div>
--- components/header.jsx
+++ components/header.jsx
@@ -17,13 +17,14 @@ function header() {
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
-          <Image
+          {/* <Image
             src={'next.svg'}
             alt="logo"
             width={20}
             height={20}
             className="h-4 w-auto object-contain"
-          />
+          /> */}
+          <span className="font-bold text-xl">TrackXpense</span>
         </Link>
         <div className="flex items-center space-x-4">
           <SignedIn>
