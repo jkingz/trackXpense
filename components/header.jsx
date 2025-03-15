97b38f9cb6fa5092d4e93f42a99027430378dcbe
Jkingz <jfkrule@gmail.com>
2025-03-16 04:12:59 +0800

    fix: Fix checkuser function

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {
   SignedIn,
   SignedOut,
@@ -8,22 +6,17 @@ import {
   UserButton,
 } from '@clerk/nextjs';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Button } from './ui/button';
 import { LayoutDashboard, PenBox } from 'lucide-react';
 
-function header() {
+import { checkUser } from '@/lib/checkUser';
+
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
-          {/* <Image
-            src={'next.svg'}
-            alt="logo"
-            width={20}
-            height={20}
-            className="h-4 w-auto object-contain"
-          /> */}
           <span className="font-bold text-xl">TrackXpense</span>
         </Link>
         <div className="flex items-center space-x-4">
@@ -51,17 +44,17 @@ function header() {
           </SignedOut>
           <SignedIn>
             <UserButton
-            appearance={{
-              elements: {
-                avatarBox: 'w-10 h-10',
-              },
-            }}
+              appearance={{
+                elements: {
+                  avatarBox: 'w-10 h-10',
+                },
+              }}
             />
           </SignedIn>
         </div>
       </nav>
     </div>
   );
-}
+};
 
-export default header;
+export default Header;
