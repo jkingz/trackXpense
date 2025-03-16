5b07541ad360d76778d8a31a67fa83cab767925b
Jkingz <jfkrule@gmail.com>
2025-03-17 03:44:49 +0800

    feat: Create account transaction table components

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,7 +1,46 @@
-const AccountsPage = () => {
+import { getAccountsWithTransactions } from '@/actions/accounts';
+import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
+import { BarLoader } from 'react-spinners';
+import TransactionsTable from '../_components/transaction-table';
+
+const AccountsPage = async ({ params }) => {
+  const resolvedParams = await params; // Await the params Promise
+  const accountData = await getAccountsWithTransactions(resolvedParams.id); // Use resolved id
+  if (!accountData) return notFound();
+
+  // destructure the transactions data and account data
+  const { transactions, ...account } = accountData;
+
   return (
-    <div>
-      <h1>Accounts Page</h1>
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight --gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{' '}
+            Account
+          </p>
+        </div>
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
+      </div>
+
+      {/* {Chart Section} */}
+      {/* {Transactions Table} */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={'100%'} color="#9333ea" />}
+      >
+        <TransactionsTable transactions={transactions} />
+      </Suspense>
     </div>
   );
 };
