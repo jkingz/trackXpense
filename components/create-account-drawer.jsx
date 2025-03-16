236596019a21066eb8d6adc2d3535ea5f91e784a
Jkingz <jfkrule@gmail.com>
2025-03-16 08:01:18 +0800

    feat: Added create accnt drawer component

---
+++ components/create-account-drawer.jsx
@@ -0,0 +1,189 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import useFetch from '@/hooks/use-fetch';
+import { Loader2 } from 'lucide-react';
+import { toast } from 'sonner';
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerTrigger,
+  DrawerClose,
+} from '@/components/ui/drawer';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+
+import { accountSchema } from '@/app/lib/schema';
+import { createAccount } from '@/actions/dashboard';
+
+const CreateAccountDrawer = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+    reset,
+  } = useForm({
+    resolver: zodResolver(accountSchema),
+    defaultValues: {
+      name: '',
+      type: 'CURRENT',
+      balance: '',
+      isDefault: false,
+    },
+  });
+
+  const {
+    data: newAccount,
+    error,
+    fn: createAccountFn,
+    loading: createAccountLoading,
+  } = useFetch(createAccount);
+
+  useEffect(() => {
+    if (newAccount && !createAccountLoading) {
+      toast.success('Account created successfully');
+      reset();
+      setOpen(false);
+    }
+  }, [newAccount, createAccountLoading, reset]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message);
+    }
+  }, [error]);
+
+  const onSubmit = async (data) => {
+    console.log(data.balance);
+    await createAccountFn(data);
+  };
+
+  const balanceValue = watch('balance'); // Optional: watch live value
+  console.log('Current balance:', balanceValue);
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Create New Account</DrawerTitle>
+        </DrawerHeader>
+        {/* {Account name} */}
+        <div className="px-4 pb-4">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className="space-y-2">
+              <label htmlFor="name" className="font-medium text-sm">
+                Account Name
+              </label>
+              <Input
+                id="name"
+                placeholder="e.g, John Doe"
+                {...register('name')}
+              ></Input>
+              {errors.name && (
+                <p className="text-red-500 text-sm">{errors.name.message}</p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="type" className="font-medium text-sm">
+                Account Type
+              </label>
+              <Select
+                onValueChange={(value) => setValue('type', value)}
+                defaultValue={watch('type')}
+              >
+                <SelectTrigger id="type" className="w-full">
+                  <SelectValue placeholder="Select type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="CURRENT">Current</SelectItem>
+                  <SelectItem value="SAVINGS">Savings</SelectItem>
+                </SelectContent>
+              </Select>
+              {errors.type && (
+                <p className="text-red-500 text-sm">{errors.type.message}</p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="balance"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Initial Balance
+              </label>
+              <Input
+                id="balance"
+                type="number"
+                step="0.01"
+                placeholder="0.00"
+                {...register('balance')}
+              />
+              {errors.balance && (
+                <p className="text-sm text-red-500">{errors.balance.message}</p>
+              )}
+            </div>
+            <div className="flex items-center justify-between rounded-lg border p-3 gap-2">
+              <div className="space-y-0.5">
+                <label
+                  htmlFor="isDefault"
+                  className="font-medium text-sm cursor-pointer"
+                >
+                  Set as Default
+                </label>
+                <p className="text-sm text-muted-foreground">
+                  This account will be selected by default for transactions
+                </p>
+              </div>
+              <Switch
+                id="isDefault"
+                onCheckedChange={(checked) => setValue('isDefault', checked)}
+                checked={watch('isDefault')}
+              ></Switch>
+            </div>
+            <div className="flex gap-4 justify-end items-center px-4 pb-4">
+              <DrawerClose asChild>
+                <Button type="button" variant="outline" className="flex-1">
+                  Cancel
+                </Button>
+              </DrawerClose>
+              <Button
+                type="submit"
+                className="flex-1 disabled={createAccountLoading}"
+              >
+                {createAccountLoading ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin">
+                      Creating...
+                    </Loader2>
+                  </>
+                ) : (
+                  'Create Account'
+                )}
+              </Button>
+            </div>
+          </form>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CreateAccountDrawer;
