9d822f0e4d5145f44af7f869c3c3ad7749028696
Jkingz <jfkrule@gmail.com>
2025-03-16 08:00:27 +0800

    feat: Added dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import CreateAccountDrawer from '@/components/create-account-drawer';
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
+
 const DashBoardPage = () => {
   return (
-    <div>
-      <h1>ASDASD</h1>
+    <div className="px-5 ">
+      {/* {Account Grid} */}
+      <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center h-full pt-5 text-muted-foreground">
+              <Plus className="h-10 w-10 mb-2"></Plus>
+              <p className="text-sm font-medium">Add new account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+      </div>
     </div>
   );
 };
