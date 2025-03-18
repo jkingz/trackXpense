b8d2f5c24771db219277f8bd46e936ec09fb375c
Jkingz <jfkrule@gmail.com>
2025-03-19 00:39:55 +0800

    fix: Update app dashboard layout loader

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -8,7 +8,14 @@ const DashBoardLayout = () => {
     <div className="px-5 ">
       <h1 className="text-6xl font-bold --gradient-title mb-5">Dashboard</h1>
       <Suspense
-        fallback={<BarLoader className="mt-4" width={'100%'} color="#0ea5e9" />}
+        fallback={
+          <BarLoader
+            className="mt-4"
+            width={'100%'}
+            height={5}
+            color="#0ea5e9"
+          />
+        }
       >
         <DashBoardPage />
       </Suspense>
