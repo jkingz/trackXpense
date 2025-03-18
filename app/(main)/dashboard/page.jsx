07b4cb84392833fec8ecb0eedad6c35fe6ccc526
Jkingz <jfkrule@gmail.com>
2025-03-19 00:39:09 +0800

    feat: Create app dashboard budget progress page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,29 @@
-import React from "react";
-
-import CreateAccountDrawer from "@/components/create-account-drawer";
-import { Card, CardContent } from "@/components/ui/card";
-import { Plus } from "lucide-react";
-import { getUserAccounts } from "@/actions/dashboard";
-import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from '@/actions/budget';
+import { getUserAccounts } from '@/actions/dashboard';
+import CreateAccountDrawer from '@/components/create-account-drawer';
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
+import AccountCard from './_components/account-card';
+import BudgetProgress from './_components/budget-progress';
 
 const DashBoardPage = async () => {
   const accounts = await getUserAccounts();
+
+  // find default account
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
   return (
-    <div className="px-5 ">
+    <div className="space-y-8">
+      {/* {Budgets} */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
       {/* {Account Grid} */}
       <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-4">
         <CreateAccountDrawer>
