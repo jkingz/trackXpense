5f30208053fd9a658390ca06a78af3a99897cf5d
Jkingz <jfkrule@gmail.com>
2025-03-20 05:44:23 +0800

    feat: Add inngest function for process trigger recurring transactions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,18 @@
 import { serve } from 'inngest/next';
 
 import { inngest } from '@/lib/inngest/client';
-import { checkedBudgetAlert } from '@/lib/inngest/functions';
+import {
+  checkedBudgetAlert,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from '@/lib/inngest/functions';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [checkedBudgetAlert],
+  functions: [
+    checkedBudgetAlert,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+  ],
 });
